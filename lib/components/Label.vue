7f4b9c9aace27d5a568303bdc86d2f8407d1731e
muammery <anavatan40@gmail.com>
2018-08-06 17:25:54 +0300

    Grid column and grid rows attributes added

--- lib/components/Label.vue
+++ lib/components/Label.vue
@@ -1,15 +1,54 @@
 <template>
-      <label :value="text"> </label>
-   
+  <div :style="{backgroundColor: backgroundColor, gridColumn: gridColumnStyle, gridRow: gridRowStyle}">
+    {{text}}
+  </div>
 </template>
 
 <script>
 export default {
     name: 'Label',
-    props: {
-        text: String,
-        textWrap: Boolean, // TODO
-    },  
+    props: [
+        'text',
+        'textWrap',
+        'left',
+        'top',
+        'dock',
+        'stretchLastChild',
+        'width',
+        'height',
+        'order',
+        'flexGrow',
+        'flexShrink',
+        'alignSelf',
+        'flexWrapBefore',
+        'row',
+        'col',
+        'rowSpan',
+        'colSpan',
+        'backgroundColor',
+    ],
+    data () {
+        return {
+            labelStyle: '',
+            containerStyle: '',
+        };
+    },
+    computed: {
+        gridColumnStyle: function() {
+            if(typeof this.colSpan !== 'undefined' && typeof this.col !== 'undefined'){
+                if(parseInt(this.colSpan) !== 0 && parseInt(this.col) !== 0){
+                    return (parseInt(this.col) + 1) + ' / ' + (parseInt(this.col) + parseInt(this.colSpan) + 1);
+                }
+            }
+        },
+        gridRowStyle: function() {
+            if(typeof this.rowSpan !== 'undefined' && typeof this.row !== 'undefined'){
+                if(parseInt(this.rowSpan) !== 0 && parseInt(this.row) !== 0){
+                    return (parseInt(this.row) + 1) + ' / ' + (parseInt(this.row) + parseInt(this.rowSpan) + 1);
+                }
+            }
+        }
+    }
 };
 </script>
 
