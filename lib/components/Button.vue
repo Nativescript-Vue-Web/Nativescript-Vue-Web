b45708a1b27da04d9c20a6ff04c35fed2ab8c834
muammery <anavatan40@gmail.com>
2018-08-07 11:54:36 +0300

    Absolute props added

--- lib/components/Button.vue
+++ lib/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-      <button class="nvw-button" @click="onClick" :value="text" ></button> 
+    <button class="nvw-button" @click="onClick" :class="{ 'nvw-button--text-wrap': textWrap}" :style="{width: convertPxStyle(this.width), height: convertPxStyle(this.height)}">{{text}}</button>
 </template>
 
 <script>
@@ -7,20 +7,39 @@ export default {
     name: 'Button',
     props: {
         text: String,
-        textWrap: Boolean, // TODO
+        textWrap: {
+            type: Boolean,
+            default: false
+        },
+        width: {
+            type: Number
+        },
+        height: {
+            type: Number,
+            default: 21
+        }
     },
     methods: {
         onClick() {
             this.$emit('tap');
+        },
+        convertPxStyle: function (value) {
+            if(typeof value === 'undefined')
+                return '';
+            else
+                return value.toString().includes('%') ? value : value + 'px';
         }
     }
 };
 </script>
 
 <style scoped lang="scss">
+    .nvw-button {
 
-.nvw-button {
-  border: none;
-  border-radius: 2px;
-}
+    }
+
+    .nvw-button--text-wrap {
+        width: 50px;
+        overflow: hidden;
+    }
 </style>
--- lib/layouts/AbsoluteLayout.vue
+++ lib/layouts/AbsoluteLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="nvw-absolute-layout" :style="{width: convertPxStyle(width), height: convertPxStyle(height), backgroundColor: backgroundColor}">
         <slot></slot>
     </div>
 </template>
@@ -7,17 +7,57 @@
 <script>
 export default {
     name: 'AbsoluteLayout',
+    props: {
+        width: {
+            default: '100%'
+        },
+        height: {
+            default: '100%'
+        },
+        backgroundColor: {},
+    },
+    mounted () {
+        this.$slots.default.forEach(vnode => {
+
+            const propsData = vnode.componentOptions.propsData;
+
+            for(const prop in propsData){
+                if(!propsData.hasOwnProperty(prop)) continue;
+
+                switch (prop){
+                case 'backgroundColor':
+                    vnode.elm.style.backgroundColor = propsData[prop];
+                    break;
+                case 'top':
+                    vnode.elm.style.top = this.convertPxStyle(propsData[prop]);
+                    break;
+                case 'left':
+                    vnode.elm.style.left = this.convertPxStyle(propsData[prop]);
+                    break;
+                case 'width':
+                    vnode.elm.style.width = this.convertPxStyle(propsData[prop]);
+                    break;
+                case 'height':
+                    vnode.elm.style.height = this.convertPxStyle(propsData[prop]);
+                    break;
+                }
+            }
+        });
+    },
+    methods: {
+        convertPxStyle: function (value) {
+            return value.toString().includes('%') ? value : value + 'px';
+        }
+    }
 };
 </script>
 
 <style lang="scss" scoped>
-    .container {
-        width: 500px;
-        height: 500px;
+    .nvw-absolute-layout {
         background-color: burlywood;
         position: relative;
     }
-    .container > * {
+    .nvw-absolute-layout > * {
         position: absolute;
     }
 </style>
