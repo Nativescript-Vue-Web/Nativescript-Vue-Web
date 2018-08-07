07795ccf349c997b515ac733b752a5902a56a8ca
muammery <anavatan40@gmail.com>
2018-08-07 09:58:47 +0300

    Props revised. Methods updated.

--- lib/components/ListPicker.vue
+++ lib/components/ListPicker.vue
@@ -1,8 +1,12 @@
 <template>
-    <select :value="value" ref="listpicker" @change="onSelectedIndexChange($event)" @input="updateValue()">
-        <option v-if="disabledVal" disabled :value="disabledVal">{{ disabledVal }}</option>
-        <option v-for="item in items" :key="item[valueProp]" :value="item[valueProp]">
-                {{ item[labelProp] }}
+    <select @change="onSelectedIndexChange($event)" @input="updateValue($event)" class="nvw-list-picker">
+        <option v-for="(item, index) in items"
+                v-bind:item="item"
+                v-bind:index="index"
+                v-bind:key="index"
+                :value="item"
+                :selected="selectedIndex === index">
+            {{ item }}
         </option>
     </select>
 </template>
@@ -11,18 +15,18 @@
 
 export default {
     name: 'ListPicker',
-    props: [
-        'disabledVal', // Works only in web.
-        'items',
-        'valueProp',
-        'labelProp',
-        'selectedIndex',
-        'selectedIndexChange', // an event.
-        'value',
-    ],
+    props: {
+        items: {
+            type: Array
+        },
+        selectedIndex: {
+            type: Number,
+            default: 0
+        }
+    },
     methods: {
-        updateValue: function () {
-            this.$emit('input', parseInt(this.$refs.listpicker.value));
+        updateValue: function (event) {
+            this.$emit('input', event.target.selectedIndex);
         },
         onSelectedIndexChange: function (event) {
             this.$emit('selectedIndexChange', event);
