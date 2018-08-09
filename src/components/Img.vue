31895a20175f6fcf7bfd6ed5929121671d0fc584
muammery <anavatan40@gmail.com>
2018-08-09 17:58:07 +0300

    stretchClass updated

--- src/components/Img.vue
+++ src/components/Img.vue
@@ -1,8 +1,10 @@
 <template>
-  <img :src="imageSrc" :class="stretchClass" :style="{ width: this.convertPxStyle(this.width), height: this.convertPxStyle(this.height)}" />
+  <img :src="imageSrc" :class="stretchClass" :width="width" :height="height" />
 </template>
 
 <script>
+import { camelCaseToDash } from '../helpers/helpers';
+
 export default {
   name: 'Img',
   props: {
@@ -21,33 +23,25 @@ export default {
   },
   computed: {
     stretchClass: function() {
-      return {
-        'nvw-img': this.stretch === 'none' ? true : false,
-        'nvw-img nvw-img--fill': this.stretch === 'fill' ? true : false,
-        'nvw-img nvw-img--aspect-fill': this.stretch === 'aspectFill' ? true : false,
-        'nvw-img nvw-img--aspect-fit': this.stretch === 'aspectFit' ? true : false,
-      };
-    },
-  },
-  methods: {
-    convertPxStyle: function(value) {
-      return value + 'px';
+      return `nvw-img nvw-img${this.stretch !== 'none' ? '--' + camelCaseToDash(this.stretch) : ''}`;
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
-//TODO css will rebuild
+//TODO css will rebuild for stretch.
 .nvw-img {
 }
 
-.nvw-img--fill {
-  width: inherit;
-  height: inherit;
-}
+.nvw-img {
+  &--fill {
+    width: inherit;
+    height: inherit;
+  }
 
-.nvw-img--aspect-fill {
-  width: inherit;
+  &--aspect-fill {
+    width: inherit;
+  }
 }
 </style>
