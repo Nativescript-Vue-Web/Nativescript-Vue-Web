8e42cebf26cc9ce0642c52a421d8de027b51eacd
Ulubayam <gizemulubayam@gmail.com>
2018-08-10 14:37:50 +0300

    Slider and Label component fixes

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,56 +1,25 @@
 <template>
-  <div :style="{backgroundColor: backgroundColor, gridColumn: gridColumnStyle, gridRow: gridRowStyle}">
+  <div v-common-directive class="nvw-label" :style="{'white-space': textWrap ? 'normal' : 'nowrap'}">
     {{text}}
   </div>
 </template>
 
 <script>
+import CommonDirective from '../directives/CommonDirective';
+
 export default {
   name: 'Label',
-  props: [
-    'text',
-    'textWrap',
-    'left',
-    'top',
-    'dock',
-    'stretchLastChild',
-    'width',
-    'height',
-    'order',
-    'flexGrow',
-    'flexShrink',
-    'alignSelf',
-    'flexWrapBefore',
-    'row',
-    'col',
-    'rowSpan',
-    'colSpan',
-    'backgroundColor',
-  ],
-  data() {
-    return {
-      labelStyle: '',
-      containerStyle: '',
-    };
+  props: {
+    text: String,
+    textWrap: Boolean,
   },
-  computed: {
-    gridColumnStyle: function() {
-      if (typeof this.colSpan !== 'undefined' && typeof this.col !== 'undefined') {
-        if (parseInt(this.colSpan) !== 0 && parseInt(this.col) !== 0) {
-          return parseInt(this.col) + 1 + ' / ' + (parseInt(this.col) + parseInt(this.colSpan) + 1);
-        }
-      }
-    },
-    gridRowStyle: function() {
-      if (typeof this.rowSpan !== 'undefined' && typeof this.row !== 'undefined') {
-        if (parseInt(this.rowSpan) !== 0 && parseInt(this.row) !== 0) {
-          return parseInt(this.row) + 1 + ' / ' + (parseInt(this.row) + parseInt(this.rowSpan) + 1);
-        }
-      }
-    },
+  directives: {
+    'common-directive': CommonDirective,
   },
 };
 </script>
 
 <style scoped lang='scss'>
+.nvw-label {
+}
 </style>
