411f9078583f89c4908038ea642098f8ce4fe626
Ulubayam <gizemulubayam@gmail.com>
2018-08-14 11:29:13 +0300

    Button component fixes and testing

--- tests/unit/Button.spec.js
+++ tests/unit/Button.spec.js
@@ -1,14 +1,37 @@
 import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
-
-import Button from '@/components/Button.vue';
+import { mount } from '@vue/test-utils';
+import Button from '../../src/components/Button.vue';
 
 describe('Button.vue', () => {
-  it('renders props.msg when passed', () => {
-    const text = 'Name';
-    const wrapper = shallowMount(Button, {
-      propsData: { text },
-    });
-    expect(wrapper.html()).to.include(text);
+  const text = 'initial string';
+  const textWrap = false;
+  const wrapper = mount(Button, {
+    name: 'Button',
+    props: {
+      text: String,
+      textWrap: Boolean,
+    },
+    propsData: {
+      text,
+      textWrap,
+    },
+  });
+  it(`initial value taken from text is equal to: ${text}.`, () => {
+    expect(wrapper.props().text).to.equal(text);
+  });
+  it(`textWrap property is equal to: ${textWrap}.`, () => {
+    expect(wrapper.props().textWrap).to.equal(textWrap);
+  });
+  it('click event property is passed to the component successfully.', () => {
+    wrapper.trigger('click');
+    wrapper.trigger('click');
+    expect(wrapper.emitted().tap.length).to.equal(2);
+  });
+  it(`the type attribute which is equivalent of white-space in Nativescript-vue is equal to: ${textWrap ? 'normal' : 'nowrap'}.`, () => {
+    if (textWrap) {
+      expect(wrapper.find('button').element.style.whiteSpace).to.equal('normal');
+    } else {
+      expect(wrapper.find('button').element.style.whiteSpace).to.equal('nowrap');
+    }
   });
 });
