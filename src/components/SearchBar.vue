c0c4b8382121e20fc7fbf9b5d6ef12f8f9082925
Özer Çevikaslan <ozer.cevikaslan@gmail.com>
2018-09-20 17:56:02 +0300

    improving CSS and fixing a typo in describe statement in the test.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,18 +1,18 @@
 <template>
-    <div v-common-directive :style="{backgroundColor: textFieldBackgroundColor}" class="nvw-searchbar">
-        <span @click="$emit('submit')" class="nvw-searchbar__search-icon" />
-        <input
-          ref="searchbar"
-          class="nvw-searchbar__search-input"
-          :placeholder="hint"
-          :type="keyboardType"
-          :value="text"
-          @keyup.enter="$emit('submit')"
-          @input="updateValue"
-          @change="$emit('textChange')"
-        />
-        <span v-if="text" @click="onClear" class="nvw-searchbar__clear-icon" />
-    </div>
+  <div v-common-directive :style="{backgroundColor: textFieldBackgroundColor}" class="nvw-searchbar">
+    <span @click="$emit('submit')" class="nvw-searchbar__search-icon" />
+    <input
+      ref="searchbar"
+      class="nvw-searchbar__search-input"
+      :placeholder="hint"
+      :type="keyboardType"
+      :value="text"
+      @keyup.enter="$emit('submit')"
+      @input="updateValue"
+      @change="$emit('textChange')"
+    />
+    <span v-if="text" @click="onClear" class="nvw-searchbar__clear-icon" />
+  </div>
 </template>
 
 <script>
@@ -52,16 +52,25 @@ export default {
   align-items: stretch;
   justify-content: space-between;
   min-width: 90px;
+  max-height: 75px;
   min-height: 28px;
-  padding: 5px;
+  padding: 7px;
+  border-radius: 12px;
+  box-shadow: 2px 3px grey;
 
   &__search-input {
     margin: 0;
     padding: 0;
     text-align: left;
-    font-weight: bold;
+    border: none;
+    border-right: solid 3px rgba(54, 79, 199, 0.5);
+    border-left: solid 3px rgba(54, 79, 199, 0.5);
+    box-shadow: 5px white;
     flex: 1;
     min-width: 30px;
+    &:focus {
+      box-shadow: 0 0 4px rgba(54, 79, 199, 0.5);
+    }
   }
 
   &__search-icon {
--- tests/unit/components/SearchBar.spec.js
+++ tests/unit/components/SearchBar.spec.js
@@ -46,19 +46,15 @@ describe('SearchBar Test.', () => {
     it(`initial value taken from v-model is equal to: ${text}.`, () => {
       expect(wrapper.props().text).to.equal(text);
     });
-
     it(`hint property is equal to: ${hint}.`, () => {
       expect(wrapper.props().hint).to.equal(hint);
     });
-
     it('change event property is passed to the component successfully.', () => {
       expect(wrapper.vm.$listeners.textChange).to.not.equal(undefined);
     });
-
     it('clear event property is passed to the component successfully.', () => {
       expect(wrapper.vm.$listeners.clear).to.not.equal(undefined);
     });
-
     it('pressing return key event property is passed to the component successfully.', () => {
       expect(wrapper.vm.$listeners.submit).to.not.equal(undefined);
     });
@@ -68,7 +64,6 @@ describe('SearchBar Test.', () => {
     it(`the placeholder attribute is equal to${hint}.`, () => {
       expect(wrapper.find('input').attributes().placeholder).to.equal(hint);
     });
-
     it(`the disabled attribute of the input in the SearchBar.`, () => {
       expect(wrapper.find('input').element.disabled).to.equal(false);
     });
@@ -78,7 +73,7 @@ describe('SearchBar Test.', () => {
     it('there is one input field.', () => {
       expect(wrapper.findAll('input').length).to.equal(1);
     });
-    it('there is two icons which are actually spans.', () => {
+    it('there are two icons which are actually spans.', () => {
       expect(wrapper.findAll('span').length).to.equal(2);
     });
   });
@@ -89,20 +84,17 @@ describe('SearchBar Test.', () => {
       expect(wrapper.find('input').element.value).to.equal('new value');
       expect(updateValueSpy.called).to.equal(true);
     });
-
     it('the input emits change event so, event handler named textChange gets thrown', () => {
       wrapper.find('input').trigger('change');
       expect(wrapper.emitted().textChange.length).to.equal(1);
       expect(textChange.called).to.equal(true);
     });
-
     it('the clearIcon gets clicked, the event is emitted and text becomes an empty string.', () => {
       wrapper.find('.nvw-searchbar__clear-icon').trigger('click');
       expect(onClear.called).to.equal(true);
       expect(wrapper.emitted().clear.length).to.equal(1);
       expect(clear.called).to.equal(true);
     });
-
     it('the value of the input component change to a new value again and then, the submit button gets clicked.', () => {
       // Change the value of the input field.
       wrapper.find('input').setValue('newest value');
@@ -112,8 +104,7 @@ describe('SearchBar Test.', () => {
       expect(submit.called).to.equal(true);
       expect(wrapper.emitted().submit.length).to.equal(1);
     });
-
-    it('the user pushes the enter button to return a value so, event handler named returnPress gets thrown', () => {
+    it('the user pushes the enter button to return a value so, event handler named submit gets thrown', () => {
       wrapper.find('input').trigger('keyup.enter');
       expect(wrapper.emitted().submit.length).to.equal(2);
       expect(submit.called).to.equal(true);
