f534b58cd7c5c2cdea259574baa68599624257c2
Ulubayam <gizemulubayam@gmail.com>
2018-09-21 09:55:00 +0300

    Frame component fixes and test

--- tests/unit/components/Frame.spec.js
+++ tests/unit/components/Frame.spec.js
@@ -1,15 +1,44 @@
 import { expect } from 'chai';
 import { mount } from '@vue/test-utils';
-import { Frame } from '../../../src/main';
+import { Frame, Label } from '../../../src/main';
 
 describe('Frame.vue', () => {
+  const LabelWrapper = {
+    render(h) {
+      return h(Label, {
+        props: {
+          text: 'here is the label',
+        },
+        attrs: {
+          height: 120,
+          alignSelf: 'flex-start',
+        },
+      });
+    },
+  };
   const wrapper = mount(Frame, {
     name: 'Frame',
+    slots: {
+      default: [LabelWrapper],
+    },
   });
-  describe('the component contains exactly one div and one slot.', () => {
+  describe('the component contains exactly one div.', () => {
     it('there is one div.', () => {
       expect(wrapper.contains('div')).to.equal(true);
       expect(wrapper.findAll('div').length).to.equal(1);
+    });
+  });
+
+  describe('frame component contains label component', () => {
+    it('there is one label component inside the frame.', () => {
+      const labelWrappers = wrapper.findAll(Label).wrappers;
+      expect(labelWrappers.length).to.equal(1);
+    });
+
+    it('label component displays the given text prop(`here is the label`) correctly inside the frame.', () => {
+      const labelWrappers = wrapper.findAll(Label).wrappers;
+      const label = labelWrappers[0].find(Label);
+      expect(label.element.textContent.trim()).to.equal('here is the label');
     });
   });
 });
