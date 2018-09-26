1fbffb67e347ebfabe24d02e42dfb3aa3302fdfb
Ulubayam <gizemulubayam@gmail.com>
2018-09-26 09:54:16 +0300

    SegmentedBarItem component

--- src/components/SegmentedBarItem.vue
+++ src/components/SegmentedBarItem.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
-        SegmentedBarItem
-        <slot></slot>
-    </div>
+    <Button :class="barItemClass" :text="title" @tap="$parent.chooseTab($event, currentTab)" />
 </template>
 
 <script>
+import Button from './Button.vue';
+
 export default {
   name: 'SegmentedBarItem',
-  props: ['title'],
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    currentTab: function() {
+      const { $parent } = this;
+      const { $slots } = $parent;
+      const indexOfBarItem = $slots.default.findIndex(obj => obj.componentOptions.propsData.title === this.title);
+      return indexOfBarItem;
+    },
+    barItemClass: function() {
+      return `${this.currentTab === this.$parent.selectedIndex ? 'nvw-segmented-bar-item--show' : 'nvw-segmented-bar-item--hide'}`;
+    },
+  },
+  components: {
+    Button,
+  },
 };
 </script>
+
+<style lang="scss">
+.nvw-segmented-bar-item {
+  &--show {
+    text-decoration: underline;
+    color: black;
+  }
+  &--hide {
+    color: gray;
+  }
+}
+</style>
