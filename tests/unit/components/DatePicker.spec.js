2a70be3b3f0caf746ae0a32f451160283d710b50
mylmz10 <yilmaz.muammer@outlook.com>
2018-11-19 16:34:49 +0300

    Test Updated

--- tests/unit/components/DatePicker.spec.js
+++ tests/unit/components/DatePicker.spec.js
@@ -25,7 +25,100 @@ describe('DatePicker Unit Test.', () => {
     },
   });
 
-  describe('Change the value inside input element and check the emitted events.', () => {
+  xdescribe('the component receives given props correctly.', () => {
+    it(`initial value taken from v-model is equal to: ${date}.`, done => {
+      expect(wrapper.props().date).to.equal(date);
+      done();
+    });
+    it(`minDate prop is equal to: ${minDate}.`, done => {
+      expect(wrapper.props().minDate).to.equal(minDate);
+      done();
+    });
+    it(`maxDate prop is equal to: ${maxDate}.`, done => {
+      expect(wrapper.props().maxDate).to.equal(maxDate);
+      done();
+    });
+
+    it(`min property is equal to ${minDate}.`, () => {
+      expect(wrapper.attributes().min).to.equal(
+        `${minDate.getFullYear().toString()}-${(minDate.getUTCMonth() + 1).toString().padStart(2, '0')}-${minDate
+          .getUTCDate()
+          .toString()
+          .padStart(2, '0')}`,
+      );
+    });
+
+    it(`max property is equal to ${maxDate}.`, () => {
+      expect(wrapper.attributes().max).to.equal(
+        `${maxDate.getFullYear().toString()}-${(maxDate.getUTCMonth() + 1).toString().padStart(2, '0')}-${maxDate
+          .getUTCDate()
+          .toString()
+          .padStart(2, '0')}`,
+      );
+    });
+
+    it('pressing return key event property is passed to the component successfully.', done => {
+      expect(wrapper.vm.$listeners.dateChange).to.not.equal(undefined);
+      done();
+    });
+
+    it(`day, month, year props checked`, () => {
+      const day = 2;
+      const month = 5;
+      const year = 2018;
+
+      const dateWrapper = mount(DatePicker, {
+        propsData: {
+          day,
+          month,
+          year,
+        },
+      });
+
+      expect(dateWrapper.props().day).to.equal(day);
+      expect(dateWrapper.props().month).to.equal(month);
+      expect(dateWrapper.props().year).to.equal(year);
+    });
+
+    it(`day, month, year props empty`, () => {
+      const dateWrapper = mount(DatePicker);
+      const date = new Date();
+      const day = date.getUTCDate();
+      const month = date.getUTCMonth();
+      const year = date.getFullYear();
+
+      expect(dateWrapper.element.value).to.equal(`${year}-${month + 1}-${day}`);
+    });
+  });
+
+  xdescribe('the input inside the component has attributes with correct values.', () => {
+    it(`the maxDate attribute is equal to${maxDate}.`, done => {
+      const maxDateAttr = wrapper.find('input').attributes().max;
+      expect(new Date(maxDateAttr).toLocaleDateString()).to.equal(maxDate.toLocaleDateString());
+      done();
+    });
+    it(`the minDate attribute is equal to${minDate}.`, done => {
+      const minDateAttr = wrapper.find('input').attributes().min;
+      expect(new Date(minDateAttr).toLocaleDateString()).to.equal(minDate.toLocaleDateString());
+      done();
+    });
+  });
+
+  xdescribe('Changing the prop and then check the value inside the input element.', () => {
+    it('Changing the maxDate property.', done => {
+      wrapper.setProps({ maxDate: new Date('2018-08-22') });
+      const maxDateProp = wrapper.props().maxDate.toLocaleDateString();
+      expect(maxDateProp).to.equal(new Date('2018-08-22').toLocaleDateString());
+      done();
+    });
+    it('Changing the minDate property.', done => {
+      wrapper.setProps({ minDate: new Date('2014-08-22') });
+      const minDateProp = wrapper.props().minDate.toLocaleDateString();
+      expect(minDateProp).to.equal(new Date('2014-08-22').toLocaleDateString());
+      done();
+    });
+  });
+  xdescribe('Change the value inside input element and check the emitted events.', () => {
     it('The value inside the input will be set to "2017-08-22" and the dateChange event will be emitted.', done => {
       wrapper.find('input').setValue(new Date('2017-08-22').toISOString().split('T', 1)[0]);
       expect(updateValueSpy.called).to.equal(true);
