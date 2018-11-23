18f7b5104fd3eb99536f98749c83d669f8f25afa
Özer <ozer.cevikaslan@gmail.com>
2018-11-23 10:12:01 +0300

    A bit styling on searchbar.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -63,30 +63,27 @@ export default {
 .nvw-searchbar {
   display: flex;
   align-items: stretch;
+  background-color: #908f8f;
   justify-content: space-between;
   min-width: 90px;
   max-height: 75px;
   min-height: 28px;
   padding: 7px;
   border-radius: 12px;
-  box-shadow: 2px 3px grey;
 
   &__search-input {
     margin: 0;
     padding: 0;
     text-align: left;
-    border: none;
-    border-right: solid 3px rgba(54, 79, 199, 0.5);
-    border-left: solid 3px rgba(54, 79, 199, 0.5);
-    box-shadow: 5px white;
     flex: 1;
     min-width: 30px;
     &:focus {
-      box-shadow: 0 0 4px rgba(54, 79, 199, 0.5);
+      box-shadow: 1px 1px rgba(54, 79, 199, 0.5);
     }
   }
 
   &__search-icon {
+    cursor: pointer;
     background-image: url('../assets/icons/searchIcon.png');
     background-size: 75%;
     background-repeat: no-repeat;
@@ -95,6 +92,7 @@ export default {
   }
 
   &__clear-icon {
+    cursor: pointer;
     background-image: url('../assets/icons/clearIcon.png');
     background-size: 75%;
     background-repeat: no-repeat;
