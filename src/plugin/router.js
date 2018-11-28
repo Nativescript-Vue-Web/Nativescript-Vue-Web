d67b83fd4b3124684eff8678980ebfffbe0756fe
mylmz10 <yilmaz.muammer@outlook.com>
2018-11-28 14:09:42 +0300

    Router update

--- src/plugin/router.js
+++ src/plugin/router.js
@@ -1,8 +1,10 @@
 const NvwRouter = {
   install: (Vue, pluginOptions) => {
-    // Show Modal
+    let history = [];
+
     Vue.prototype.$navigateTo = function(component) {
-      // eslint-disable-line
+      history.push(this);
+      console.log(history);// eslint-disable-line
 
       if (pluginOptions && pluginOptions.router) {
         pluginOptions.router.push(component);
@@ -13,6 +15,28 @@ const NvwRouter = {
 
         this.$root.$el.innerHTML = '';
         this.$root.$el.appendChild(modalDom.$el);
+      }
+    };
+
+    Vue.prototype.$navigateBack = function() {
+      if (pluginOptions && pluginOptions.router) {
+        pluginOptions.router.go(-1);
+      } else {
+        console.log(history); // eslint-disable-line
+        console.log(this);// eslint-disable-line
+
+        /*
+        const component = history.pop();
+        //const ContentComponent = Vue.extend(component);
+        //const ContentInstance = new ContentComponent();
+        //const modalDom = ContentInstance.$mount();
+        //console.log(modalDom)
+        const modalDom = component.$mount();
+        console.log(component);
+
+        //this.$root.$el.innerHTML = '';
+        //this.$root.$el.appendChild(modalDom.$el);
+        */
       }
     };
   },
