c089a6b11ee8bd49ecb6540c8ce23c00427085ce
ercang <ercangercek@gmail.com>
2018-12-13 15:01:34 +0300

    updated NvwRouter implementation

--- src/plugin/router.js
+++ src/plugin/router.js
@@ -1,43 +1,19 @@
 const NvwRouter = {
   install: (Vue, pluginOptions) => {
-    let history = [];
+    if(typeof pluginOptions !== 'object' || pluginOptions.router === undefined)
+    {
+      throw "NvwRouter: Router must be defined!";
+    }
+
+    let router = pluginOptions.router;
 
     Vue.prototype.$navigateTo = function(component) {
-      history.push(this);
-      console.log(history);// eslint-disable-line
-
-      if (pluginOptions && pluginOptions.router) {
-        pluginOptions.router.push(component);
-      } else {
-        const ContentComponent = Vue.extend(component);
-        const ContentInstance = new ContentComponent();
-        const modalDom = ContentInstance.$mount();
-
-        this.$root.$el.innerHTML = '';
-        this.$root.$el.appendChild(modalDom.$el);
-      }
+      router.push(component);
+      
     };
 
     Vue.prototype.$navigateBack = function() {
-      if (pluginOptions && pluginOptions.router) {
-        pluginOptions.router.go(-1);
-      } else {
-        console.log(history); // eslint-disable-line
-        console.log(this);// eslint-disable-line
-
-        /*
-        const component = history.pop();
-        //const ContentComponent = Vue.extend(component);
-        //const ContentInstance = new ContentComponent();
-        //const modalDom = ContentInstance.$mount();
-        //console.log(modalDom)
-        const modalDom = component.$mount();
-        console.log(component);
-
-        //this.$root.$el.innerHTML = '';
-        //this.$root.$el.appendChild(modalDom.$el);
-        */
-      }
+      router.go(-1);
     };
   },
 };
